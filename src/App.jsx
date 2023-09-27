01a543b22f645082384a5390a418b1b4af5988ac
= <=>
2023-09-27 10:42:14 +0200

    FIxat de olika uppgifterna

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,44 @@ function App() {
   const [weatherData, setWeatherData] = useState([])
 
   useEffect(() => {
-    console.log(`before call: ${typeof (weatherData)}`)
     async function fetchWeather() {
       navigator.geolocation.getCurrentPosition((position) => {
         setLat(position.coords.latitude)
         setLong(position.coords.longitude)
       })
-      console.log(lat, long)
+
       if (lat === 0 || long === 0) return
-    
+
+      const usersLocation = { lat: lat, long: long }
+      const storedLocation = JSON.parse(localStorage.getItem('userslocation'));
+      const storedWeatherData = JSON.parse(localStorage.getItem('weatherdata'));
+      const storedTimestamp = localStorage.getItem('timestamp');
+      const now = new Date().getTime();
+      const tenMinutes = 10 * 60 * 1000;
+
+      if (storedLocation && JSON.stringify(usersLocation) === JSON.stringify(storedLocation)) {
+        console.log('Location is the same as last time')
+        if (storedWeatherData && now - storedTimestamp < tenMinutes) {
+          console.log('Data is less than 10 minutes old, using stored data')
+          setWeatherData(storedWeatherData);
+          return;
+        }
+      }
+
+      console.log('Fetching new data')
       const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?'
       await fetch(`${baseUrl}lat=${lat}&lon=${long}&appid=${import.meta.env.VITE_API_KEY}`)
         .then(res => res.json())
         .then(result => {
-          console.log(result)
           setWeatherData(result)
+          localStorage.setItem('weatherdata', JSON.stringify(result));
+          localStorage.setItem('userslocation', JSON.stringify(usersLocation));
+          localStorage.setItem('timestamp', now);
         }).catch(err => {
           console.log(err)
         })
     }
+
     fetchWeather()
   }, [lat, long])
 
@@ -41,7 +60,6 @@ function App() {
       ) : (
         <Spinner />
       )}
-      
     </>
   )
 }
